fe785f481d304db912a8f105dcb350cf7aa5bdd4
Martin Kollar <martin.kollar@binconf.com>
2023-08-13 03:02:14 -0700

    initial commit

---
+++ word_file_generator.py
@@ -0,0 +1,14 @@
+import win32com.client as win32
+
+# Create an instance of word
+WordApp = win32.gencache.EnsureDispatch('Shell.Application')
+WordApp
+# Make the app visible
+WordApp.Visible = True
+WordDoc = WordApp.Documents.Add()
+
+WordDoc = win32.gencache.EnsureDispatch(WordApp.Documents(1))
+
+help(WordDoc)
+
+import win32com.client
