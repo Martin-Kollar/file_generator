cac4d9873abf07434db68d8d9ab6c329e8345e3c
Martin Kollar <martin.kollar@binconf.com>
2023-08-14 05:34:22 -0700

    Updated file convertion

--- word_file_generator.py
+++ word_file_generator.py
@@ -29,7 +29,7 @@ def insert_macro_from_file(macro_path, target_app):
     target_macros = target_app.VBE.VBProjects(1).VBComponents
     new_macro = target_macros.Add(1)
     new_macro.CodeModule.AddFromString(macro_content)
-
+    
 def main(template_id, macro_id):
     # Get template path from config using template_id
     template_name = cfg['templates']['word'][template_id]
@@ -38,29 +38,22 @@ def main(template_id, macro_id):
     # Load the Word template (template with the associated macro)
     word_app, template_doc = load_word_template(template_path)
 
-    # Create a new document based on the template
-    new_doc = template_doc.NewDocument()
+    # Save the new document in the output folder
+    new_doc_path = os.path.join(output_dir, os.path.splitext(os.path.basename(template_name))[0])
+    new_doc_path += ".docm"
+    template_doc.SaveAs(new_doc_path, FileFormat=13)
     template_doc.Close()
 
-    # Save the new document in the output folder
-    new_doc_path = os.path.join(output_dir, "new_document.docx")
-    new_doc.SaveAs(new_doc_path)
-
-    # Close the new document
-    new_doc.Close()
-
-    # Re-open the newly created document
+    # Open the newly created document
     new_doc = word_app.Documents.Open(new_doc_path)
 
     # Insert macro from text file into the new document
     macro_name = cfg['macros'][macro_id]
     macro_path = os.path.join(template_dir, "macros", macro_name)
-    insert_macro_from_file(macro_path, new_doc.Application)
+    insert_macro_from_file(macro_path, new_doc)
 
     # Save the output document with the macro inserted
-    output_filename = os.path.splitext(os.path.basename(template_name))[0]
-    output_path = os.path.join(output_dir, output_filename)
-    new_doc.SaveAs(output_path + "_output.docm", FileFormat=16)
+    new_doc.Save()
 
     # Close the documents
     new_doc.Close()
